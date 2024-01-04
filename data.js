a675e4281e1891fce213f578715adbf6ded91670
EkaterinaNattrass <cattiva2012@gmail.com>
2024-01-04 21:52:28 +0100

    adds the home page and improves the cards pages

--- data.js
+++ data.js
@@ -1,46 +1,186 @@
 const cards = [
-    {
-        front: 'eple',
-        back: 'apple'
-    },
-    {
-        front: 'melk',
-        back: 'milk'
-    },
-    {
-        front: 'hvit',
-        back: 'white'
-    },
-    {
-        front: 'bord',
-        back: 'table'
-    },
-    {
-        front: 'seng',
-        back: 'bed'
-    },
-    {
-        front: 'rosa',
-        back: 'pink'
-    },
-    {
-        front: 'benk',
-        back: 'bench'
-    },
-    {
-        front: 'stol',
-        back: 'chair'
-    },
-    {
-        front: 'kjole',
-        back: 'dress'
-    },
-    {
-        front: 'jakke',
-        back: 'jacket'
-    },
-    {
-        front: 'bukse',
-        back: 'trousers'
-    }
+     [
+        {
+            front: 'eple',
+            back: 'apple'
+        },
+        {
+            front: 'melk',
+            back: 'milk'
+        },
+        {
+            front: 'brød',
+            back: 'bread'
+        },
+        {
+            front: 'kjøtt',
+            back: 'meat'
+        },
+        {
+            front: 'grønnsaker',
+            back: 'vegetables'
+        },
+        {
+            front: 'fisk',
+            back: 'fish'
+        },
+        {
+            front: 'is',
+            back: 'ice-cream'
+        },
+        {
+            front: 'syltetøy',
+            back: 'jam'
+        },
+        {
+            front: 'vann',
+            back: 'water'
+        },
+        {
+            front: 'sjokolade',
+            back: 'chocolate'
+        },
+        {
+            front: 'pølse',
+            back: 'sausage'
+        }
+    ],
+    [
+        {
+            front: 'sokk',
+            back: 'sock'
+        },
+        {
+            front: 'sko',
+            back: 'shoe'
+        },
+        {
+            front: 'genser',
+            back: 'jumper'
+        },
+        {
+            front: 'lue',
+            back: 'hat'
+        },
+        {
+            front: 'slips',
+            back: 'tie'
+        },
+        {
+            front: 'støvel',
+            back: 'boot'
+        },
+        {
+            front: 'shorts',
+            back: 'shorts'
+        },
+        {
+            front: 'T-skjorte',
+            back: 't-shirt'
+        },
+        {
+            front: 'kjole',
+            back: 'dress'
+        },
+        {
+            front: 'jakke',
+            back: 'jacket'
+        },
+        {
+            front: 'bukse',
+            back: 'trousers'
+        }
+    ],
+     [
+        {
+            front: 'gul',
+            back: 'yellow'
+        },
+        {
+            front: 'oransje',
+            back: 'orange'
+        },
+        {
+            front: 'hvit',
+            back: 'white'
+        },
+        {
+            front: 'grønn',
+            back: 'green'
+        },
+        {
+            front: 'blå',
+            back: 'blue'
+        },
+        {
+            front: 'rosa',
+            back: 'pink'
+        },
+        {
+            front: 'rød',
+            back: 'red'
+        },
+        {
+            front: 'svart',
+            back: 'black'
+        },
+        {
+            front: 'brun',
+            back: 'brown'
+        },
+        {
+            front: 'grå',
+            back: 'grey'
+        },
+        {
+            front: 'lilla',
+            back: 'purple'
+        }
+    ],
+    [
+        {
+            front: 'pute',
+            back: 'pillow'
+        },
+        {
+            front: 'hylle',
+            back: 'shelf'
+        },
+        {
+            front: 'sofa',
+            back: 'sofa'
+        },
+        {
+            front: 'bord',
+            back: 'table'
+        },
+        {
+            front: 'seng',
+            back: 'bed'
+        },
+        {
+            front: 'kjøleskap',
+            back: 'fridge'
+        },
+        {
+            front: 'benk',
+            back: 'bench'
+        },
+        {
+            front: 'stol',
+            back: 'chair'
+        },
+        {
+            front: 'badekar',
+            back: 'bathtub'
+        },
+        {
+            front: 'garderobe',
+            back: 'wardrobe'
+        },
+        {
+            front: 'skuff',
+            back: 'drawer'
+        }
+    ]
 ];
