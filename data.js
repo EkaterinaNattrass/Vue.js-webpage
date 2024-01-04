de954836691f5a51a7cad4dbd44c13715759849f
EkaterinaNattrass <cattiva2012@gmail.com>
2024-01-04 14:47:23 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,46 @@
+const cards = [
+    {
+        front: 'eple',
+        back: 'apple'
+    },
+    {
+        front: 'melk',
+        back: 'milk'
+    },
+    {
+        front: 'hvit',
+        back: 'white'
+    },
+    {
+        front: 'bord',
+        back: 'table'
+    },
+    {
+        front: 'seng',
+        back: 'bed'
+    },
+    {
+        front: 'rosa',
+        back: 'pink'
+    },
+    {
+        front: 'benk',
+        back: 'bench'
+    },
+    {
+        front: 'stol',
+        back: 'chair'
+    },
+    {
+        front: 'kjole',
+        back: 'dress'
+    },
+    {
+        front: 'jakke',
+        back: 'jacket'
+    },
+    {
+        front: 'bukse',
+        back: 'trousers'
+    }
+];
